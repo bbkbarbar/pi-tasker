eb33f7e2e421bc3c13f964bbac8eb01cdb409ba8
bbkbarbar <bbk.barbar@gmail.com>
2016-11-30 16:44:07 +0100

    .

--- TaskerApp/src/main/java/hu/barbar/tasker/Tasker.java
+++ TaskerApp/src/main/java/hu/barbar/tasker/Tasker.java
@@ -42,7 +42,7 @@ import hu.barbar.util.logger.Log;
 
 public class Tasker {
 	
-	private static final int buildNum = 96;
+	private static final int buildNum = 97;
 	
 	public static final boolean DEBUG_MODE = false;
 	
