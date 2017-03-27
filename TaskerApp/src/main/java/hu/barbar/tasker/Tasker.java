6644fdf3b86232b6091689b3ef8e4ed59aa19168
bbkbarbar <bbk.barbar@gmail.com>
2017-03-27 14:54:18 +0200

    Reaf logLevels from multi-level JSON config

--- TaskerApp/src/main/java/hu/barbar/tasker/Tasker.java
+++ TaskerApp/src/main/java/hu/barbar/tasker/Tasker.java
@@ -91,16 +91,20 @@ public class Tasker {
 				+ ")\nBuild: " + buildNum + "\n");/**/
 		
 		
-		// Read base-parameters
+		// Read base-parameters from JSON
 		int logLevelStdOut = Log.Level.INFO;
 		int logLevelFiledOut = Log.Level.WARN;
+		
 		JSONObject baseConf = FileHandler.readJSON(Env.NAME_OF_DATA_FOLDER + Env.getPathSeparator() + Env.BASE_CONFIG_FILE);
 		if(baseConf != null){
-			if(baseConf.containsKey("loglevels-stdout")){
-				logLevelStdOut = Log.getLogLevelFromString((String)baseConf.get("loglevels-stdout"));
-			}
-			if(baseConf.containsKey("loglevels-fileout")){
-				logLevelFiledOut = Log.getLogLevelFromString((String)baseConf.get("loglevels-fileout"));
+			if(baseConf.containsKey("loglevels")){
+				JSONObject logLevels = (JSONObject) baseConf.get("loglevels");
+				if(logLevels.containsKey("stdout")){
+					logLevelStdOut = Log.getLogLevelFromString((String)logLevels.get("stdout"));
+				}
+				if(baseConf.containsKey("fileout")){
+					logLevelFiledOut = Log.getLogLevelFromString((String)logLevels.get("fileout"));
+				}
 			}
 		}
 			
