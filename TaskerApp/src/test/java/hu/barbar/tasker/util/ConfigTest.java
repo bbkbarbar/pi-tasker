95a1122602dcc05a1ccf8d8b1e4538d25e170c92
aboor <v-aboor@hotels.com>
2017-03-29 16:36:16 +0200

    Tests added for reading single- and multi-level configValues from ConfigJSON

--- TaskerApp/src/test/java/hu/barbar/tasker/util/ConfigTest.java
+++ TaskerApp/src/test/java/hu/barbar/tasker/util/ConfigTest.java
@@ -11,7 +11,7 @@ import junit.framework.TestSuite;
 
 public class ConfigTest extends TestSuite {
 	
-	private static String configJsonFilePath = "..\\onTarget\\home_-_pi\\taskerData\\baseConfig.json";
+	private static String configJsonFilePath = "..\\sample JSONs\\baseConfig_For_Test.json";
 	
 	JSONObject json = null;
 	
@@ -19,9 +19,34 @@ public class ConfigTest extends TestSuite {
 	
 	
 	@Test
-	public void readConfigFromJSONTest1(){
+	public void read_NON_EXISTING_config_from_JSON_Test(){
 		
 		Config.setConfigSourceJSON(configJsonFilePath);
+		
+		String jsonPathOfWantedValue = "non-existing-key";
+		
+		String value = (String) Config.getConfig(jsonPathOfWantedValue);
+		assertEquals(null, value);
+		
+	}
+	
+	@Test
+	public void read_single_level_config_value_from_from_config_JSON_Test(){
+		
+		Config.setConfigSourceJSON(configJsonFilePath);
+		
+		String jsonPathOfWantedValue = "testkey";
+		
+		String value = (String) Config.getConfig(jsonPathOfWantedValue);
+		assertEquals("good_value", value);
+		
+	}
+	
+	@Test
+	public void read_multi_level_config_value_from_from_config_JSON_Test(){
+		
+		Config.setConfigSourceJSON(configJsonFilePath);
+		
 		String jsonPathOfWantedValue = "loglevels.stdout";
 		
 		String value = (String) Config.getConfig(jsonPathOfWantedValue);
