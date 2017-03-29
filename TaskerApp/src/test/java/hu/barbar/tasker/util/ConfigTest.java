18622e3e27841d17cd6e7edaecaa65b13b1b868b
bbkbarbar <bbk.barbar@gmail.com>
2017-03-29 19:25:58 +0200

    Fix typos

--- TaskerApp/src/test/java/hu/barbar/tasker/util/ConfigTest.java
+++ TaskerApp/src/test/java/hu/barbar/tasker/util/ConfigTest.java
@@ -10,51 +10,51 @@ import hu.barbar.util.FileHandler;
 import junit.framework.TestSuite;
 
 public class ConfigTest extends TestSuite {
-	
+
 	private static String configJsonFilePath = "..\\sample JSONs\\baseConfig_For_Test.json";
-	
+
 	JSONObject json = null;
-	
+
 	//System.out.println(FileHandler.readJSON(configJsonFilePath));
-	
-	
+
+
 	@Test
 	public void read_NON_EXISTING_config_from_JSON_Test(){
-		
+
 		Config.setConfigSourceJSON(configJsonFilePath);
-		
+
 		String jsonPathOfWantedValue = "non-existing-key";
-		
+
 		String value = (String) Config.getConfig(jsonPathOfWantedValue);
 		assertEquals(null, value);
-		
+
 	}
-	
+
 	@Test
-	public void read_single_level_config_value_from_from_config_JSON_Test(){
-		
+	public void read_single_level_config_value_from_config_JSON_Test(){
+
 		Config.setConfigSourceJSON(configJsonFilePath);
-		
+
 		String jsonPathOfWantedValue = "testkey";
-		
+
 		String value = (String) Config.getConfig(jsonPathOfWantedValue);
 		assertEquals("good_value", value);
-		
+
 	}
-	
+
 	@Test
-	public void read_multi_level_config_value_from_from_config_JSON_Test(){
-		
+	public void read_multi_level_config_value_from_config_JSON_Test(){
+
 		Config.setConfigSourceJSON(configJsonFilePath);
-		
+
 		String jsonPathOfWantedValue = "loglevels.stdout";
 		String value = (String) Config.getConfig(jsonPathOfWantedValue);
 		assertEquals("info", value);
-		
+
 		jsonPathOfWantedValue = "loglevels.fileout";
 		value = (String) Config.getConfig(jsonPathOfWantedValue);
 		assertEquals("warn", value);
-		
+
 	}
-	
+
 }
