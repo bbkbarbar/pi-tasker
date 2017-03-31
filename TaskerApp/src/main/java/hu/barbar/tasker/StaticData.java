20efae2dd66b5de2fba99ed6fd592551b17e4dfa
bbkbarbar <bbk.barbar@gmail.com>
2017-03-31 06:08:04 +0200

    BuildNumber moved to a separated class for clear git history

---
+++ TaskerApp/src/main/java/hu/barbar/tasker/StaticData.java
@@ -0,0 +1,7 @@
+package hu.barbar.tasker;
+
+public class StaticData {
+	
+	public static final int BUILD_NUM = 108;
+	
+}
--- TaskerApp/src/main/java/hu/barbar/tasker/Tasker.java
+++ TaskerApp/src/main/java/hu/barbar/tasker/Tasker.java
@@ -42,7 +42,7 @@ import hu.barbar.util.logger.Log;
 
 public class Tasker {
 
-	private static final int buildNum = 107;
+	private static final int buildNum = StaticData.BUILD_NUM;
 
 	public static final boolean DEBUG_MODE = false;
 
